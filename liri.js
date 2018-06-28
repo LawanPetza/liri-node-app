e3702b83bc54fb3e6ef7e5bdf60509a1ef3de2ad
Lawan Petza <Lawan.Petza@gmail.com>
2018-06-28 01:53:10 -0400

    modified liri.js

--- liri.js
+++ liri.js
@@ -1,36 +1,61 @@
 require("dotenv").config();
+
+
 var Twitter = require('twitter');
-var keys = require("./keys.js")
-
-
-var spotify = new Spotify(keys.spotify)
+var Spotify = require('node-spotify-api');
+var keys = require("./keys.js");
 var client = new Twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
 
-function tweetsAPI() {
+var request = require("request");
+var fs = require("fs");
+
+var action = process.argv[2];
+var value = process.argv[3];
+
+switch (action) {
+    case "my-tweets":
+        myTweets();
+        break;
+
+    case "movie-this":
+        movieThis();
+        break;
+
+    case "spotify-this-song":
+        spotifyThis();
+        break;
+
+    case "do-what-it-says":
+        random();
+        break;
+}
+// my-tweets function
+function myTweets() {
     var params = { screen_name: 'WaanLawan' };
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
-        if (!error) {
+        if (!error && response.statusCode === 200) {
             // console.log(tweets[0]);
             for (var i = 0; i < tweets.length; i++) {
                 console.log(tweets[i].created_at);
+
                 console.log(tweets[i].text)
             }
 
         }
     });
 
-}
+} // end myTweets function
 
-var request = require("request");
-function movieAPI() {
-
+// movieThis function
+function movieThis() {
     // Store all of the arguments in an array
     var nodeArgs = process.argv;
-    console.log("this is the command line " + process.argv);
+    // console.log("this is the command line " + process.argv);
 
     // Create an empty variable for holding the movie name
-
     var movieName = "";
+
     // Loop through all the words in the node argument
     // And do a little for-loop magic to handle the inclusion of "+"s
     for (var i = 3; i < nodeArgs.length; i++) {
@@ -44,7 +69,7 @@ function movieAPI() {
         }
     }
 
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&tomatoes=true&y=&plot=short&apikey=trilogy";
     console.log(movieName);
     console.log(queryUrl);
 
@@ -54,7 +79,7 @@ function movieAPI() {
         console.log("Title of the movie: " + JSON.parse(body).Title);
         console.log("Release Year: " + JSON.parse(body).Year);
         console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-        console.log("Rotten Tomatoes Rating is: " + JSON.parse(body).Rating);
+        console.log("Rotten Tomatoes Rating is: " + JSON.parse(body).TomatoRating);
         console.log("Country: " + JSON.parse(body).Country);
         console.log("Language: " + JSON.parse(body).Language);
         console.log("Plot: " + JSON.parse(body).Plot);
@@ -62,11 +87,35 @@ function movieAPI() {
 
     });
 }
-if (process.argv[2] === "movie-this") {
-    movieAPI()
+function random() {
+    fs.readFile('random.txt', 'utf8', function (error, data) {
+        if (error) {
+            console.log(error);
+        } else {
+            var dataArr = data.split(" , ");
+
+            if (dataArr[0] === "spotify-this-song") {
+                spotifyThis(dataArr[1]);
+            }
+            if (dataArr[0] === "movie-this") {
+                omdbThis(dataArr[1]);
+            }
+        }
+    });
+} // end doWhatItSays function
+function spotifyThis() {
+    spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log(data);
+        console.log("Artist: " + JSON.parse(body).Artist);
+        console.log("The song's name: " + JSON.parse(body).songName);
+        console.log("A preview link of the song from Spotify: " + JSON.parse(body).preview_URL);
+        console.log("The album that the song is from: " + JSON.parse(body).Album);
+    });
 }
-else if (process.argv[2] === "my-tweets") {
-    tweetsAPI()
-}
-console.log(process.argv);
-console.log(process.argv);
+
+
+
+
